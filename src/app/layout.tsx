0af7ac77ce8f706b874496ee4cb8321a593d53ed
Brahim Taibaoui <brahimbrah343@gmail.com>
2024-08-29 07:31:52 +0100

    Create Banner section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import clsx from "clsx";
 import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
-import clsx from "clsx";
 import "./globals.css";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
 import { Banner } from "@/components/Banner";
-import { Navbar } from "@/components/Navbar";
-import { Hero } from "@/components/Hero";
-import { LogoTicker } from "@/components/LogoTicker";
-import { Features } from "@/components/Features";
-import { ProductShowcase } from "@/components/ProductShowcase";
-import { FAQs } from "@/components/FAQs";
-import { CallToAction } from "@/components/CallToAction";
-import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
     <>
       <Banner />
-      <Navbar />
-      <Hero />
-      <LogoTicker />
-      <Features />
-      <ProductShowcase />
-      <FAQs />
-      <CallToAction />
-      <Footer />
     </>
   );
 }
